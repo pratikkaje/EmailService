ada4dba76cc9d1de420c2385671ad8b9afd34ed0
Pratik Kaje <kajepratik@gmail.com>
2025-03-31 14:05:47 +0400

    Implement SendGrid Email Service

---
+++ EmailService.Application/Interfaces/IEmailService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmailService.Domain.Models;
+
+namespace EmailService.Application.Interfaces
+{
+    public interface IEmailService
+    {
+        Task<bool> SendEmailAsync(EmailRequest emailRequest);
+    }
+}
---
+++ EmailService.Application/Services/SendGridEmailService.cs
@@ -0,0 +1,30 @@
+﻿using EmailService.Application.Interfaces;
+using EmailService.Domain.Models;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace EmailService.Application.Services
+{
+    public class SendGridEmailService : IEmailService
+    {
+        private readonly string _sendGridApiKey;
+
+        public SendGridEmailService(IConfiguration configuration)
+        {
+            _sendGridApiKey = configuration["SendGrid:ApiKey"] ?? throw new ArgumentNullException("SendGrid API Key is missing.");
+        }
+
+        public async Task<bool> SendEmailAsync(EmailRequest emailRequest)
+        {
+            var client = new SendGridClient(_sendGridApiKey);
+            var from = new EmailAddress("your-email@example.com", "App Support");
+            var to = new EmailAddress(emailRequest.To);
+            var msg = MailHelper.CreateSingleEmail(from, to, emailRequest.Subject, emailRequest.Body, emailRequest.IsHtml ? emailRequest.Body : null);
+
+            var response = await client.SendEmailAsync(msg);
+            return response.StatusCode is System.Net.HttpStatusCode.OK or System.Net.HttpStatusCode.Accepted;
+        }
+    }
+}
