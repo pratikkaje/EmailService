40cd730fe5d2bfe69ea0b3f73e6999415ccb9acd
Pratik Kaje <kajepratik@gmail.com>
2025-03-31 16:14:10 +0400

    CODE RUB: Moved From Email to Appsettings instead of hardcoding in code.

--- EmailService.Infrastructure/Services/SendGridEmailService.cs
+++ EmailService.Infrastructure/Services/SendGridEmailService.cs
@@ -9,16 +9,18 @@ namespace EmailService.Infrastructure.Services
     public class SendGridEmailService : IEmailService
     {
         private readonly string _sendGridApiKey;
+        private readonly string _sendGridFromEmail;
 
         public SendGridEmailService(IConfiguration configuration)
         {
             _sendGridApiKey = configuration["SendGrid:ApiKey"] ?? throw new ArgumentNullException("SendGrid API Key is missing.");
+            _sendGridFromEmail = configuration["SendGrid:FromEmail"] ?? throw new ArgumentNullException("From Email is missing.");
         }
 
         public async Task<bool> SendEmailAsync(EmailRequest emailRequest)
         {
             var client = new SendGridClient(_sendGridApiKey);
-            var from = new EmailAddress("your-email@example.com", "Your Name");
+            var from = new EmailAddress(_sendGridFromEmail, "App Support");
             var to = new EmailAddress(emailRequest.To);
             var msg = MailHelper.CreateSingleEmail(from, to, emailRequest.Subject, emailRequest.Body, emailRequest.IsHtml ? emailRequest.Body : null);
 
